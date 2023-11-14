099cb0f7791f6ddf309cc4a936f81b4d5d8b6272
Mohamed Nabil <mohamednabil00000@gmail.com>
2023-11-14 21:36:49 +0200

    Created data structure to calc charged minimum fees for old orders imported through csv

--- spec/support/shared_contexts/order.rb
+++ spec/support/shared_contexts/order.rb
@@ -24,3 +24,22 @@ RSpec.shared_context 'group of orders in prev month' do
                    created_at: Date.current)
   end
 end
+
+RSpec.shared_context 'group of orders in 2 months ago' do
+  let!(:order1) do
+    create(:order, merchant_reference: merchant1.reference, id: '12', amount: 10_000.0, commission: 100.0,
+                   created_at: 2.month.ago)
+  end
+  let!(:order2) do
+    create(:order, merchant_reference: merchant3.reference, id: '34', amount: 10_000.0, commission: 500.0,
+                   created_at: 2.month.ago)
+  end
+  let!(:order3) do
+    create(:order, merchant_reference: merchant2.reference, id: '45', amount: 10_000.0, commission: 500.0,
+                   created_at: 2.month.ago)
+  end
+  let!(:order4) do
+    create(:order, merchant_reference: merchant3.reference, id: '47', amount: 10_000.0, commission: 500.0,
+                   created_at: Date.current)
+  end
+end
